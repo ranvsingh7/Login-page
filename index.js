2242e4e46676ee61643faca179c441ab6a042808
Ranveer <ranvsingh7@gmail.com>
2022-11-02 23:22:30 +0530

    signup page added

--- index.js
+++ index.js
@@ -1,44 +1,50 @@
-function myFunction(x){
-    if(x.type=="email"){
-        let email= document.getElementById("signin--email").className="hrStyle"
-        document.getElementById("signin--emailHeading").innerText="Email address"
+function myFunction(x) {
+    if (x.type == "email") {
+        let email = document.getElementById("signin--email").className = "hrStyle"
+        document.getElementById("signin--emailHeading").innerText = "Email address"
     }
-    else{
-        let password= document.getElementById("signin--password").className="hrStyle"
-        document.getElementById("signin--passHeading").innerText="Password"
+    else {
+        let password = document.getElementById("signin--password").className = "hrStyle"
+        document.getElementById("signin--passHeading").innerText = "Password"
     }
-    
-    
-    x.placeholder="";
+    x.placeholder = "";
     return
 }
 
-function myFunction1(x){
-    if(x.type=="email"){
-        x.placeholder="Email address"
+function myFunction1(x) {
+    if (x.type == "email") {
+        x.placeholder = "Email address"
 
     }
-    else{
-        x.placeholder="Password"
+    else {
+        x.placeholder = "Password"
     }
     document.getElementById("signin--password").classList.remove("hrStyle")
     document.getElementById("signin--email").classList.remove("hrStyle")
-    document.getElementById("signin--passHeading").innerText=""
-    document.getElementById("signin--emailHeading").innerText=""
+    document.getElementById("signin--passHeading").innerText = ""
+    document.getElementById("signin--emailHeading").innerText = ""
 
     return
 }
 
-function removeEye(){
+function removeEye() {
 
-    let type= document.getElementById("signin--passwordInput").type
+    let type = document.getElementById("signin--passwordInput").type
     let type1
     let eye
-        type == "password" ? type1="Text":type1="password"
-        type =="password" ? eye="./images/hide.png":eye="./images/view.png"
-        document.getElementById("signin--passwordInput").type=type1
-        document.getElementById("password--eye").src=eye
-    // let eye=document.getElementById("password--eye").src=="./images/eye.png"
-    // console.log(eye)
+    type == "password" ? type1 = "Text" : type1 = "password"
+    type == "password" ? eye = "./images/hide.png" : eye = "./images/view.png"
+    document.getElementById("signin--passwordInput").type = type1
+    document.getElementById("password--eye").src = eye
+}
+
+function signin(){
+    window.location="./index.html"
+}
+function signup() {
+    window.location="./signup.html"
+}
+function signIn() {
+    alert("Sign in successfully")
 }
 
